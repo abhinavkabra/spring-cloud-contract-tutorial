3069a6fc295f096d51aec458e5858421616ac273
Matthew Campbell <mjc4648@gmail.com>
2018-08-01 02:08:59 -0400

    Update contract for GET /customers to return a list

--- customer-service/src/test/resources/contracts/canGetCustomersList.groovy
+++ customer-service/src/test/resources/contracts/canGetCustomersList.groovy
@@ -9,10 +9,18 @@ org.springframework.cloud.contract.spec.Contract.make {
     response {
         status 200
         body([
-                "id": $(regex(uuid())),
-                "firstName": $(consumer("Matt"), producer(regex('[a-zA-Z.-_]+'))),
-                "lastName": $(consumer("Matt"), producer(regex('[a-zA-Z.-_]+'))),
-                "createdOn": $(consumer('2018-08-01T05:33:16+00:00'), producer(regex(iso8601WithOffset())))
+                [
+                    "id": $(regex(uuid())),
+                    "firstName": $(consumer("Matt"), producer(regex('[a-zA-Z.-_]+'))),
+                    "lastName": $(consumer("Campbell"), producer(regex('[a-zA-Z.-_]+'))),
+                    "createdOn": $(consumer("2018-08-01T05:33:16+00:00"), producer(regex(iso8601WithOffset())))
+                ],
+                [
+                    "id": $(regex(uuid())),
+                    "firstName": $(consumer("Shaun"), producer(regex('[a-zA-Z.-_]+'))),
+                    "lastName": $(consumer("Anderson"), producer(regex('[a-zA-Z.-_]+'))),
+                    "createdOn": $(consumer("2018-08-01T06:33:16+00:00"), producer(regex(iso8601WithOffset())))
+                ]
         ])
         headers {
             contentType(applicationJsonUtf8())
