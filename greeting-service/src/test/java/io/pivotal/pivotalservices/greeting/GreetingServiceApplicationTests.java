55fde419ce5af9e7788eedd23a655c24fb6b447d
Matthew Campbell <mjc4648@gmail.com>
2018-08-01 03:42:31 -0400

    Add test for event-driven greeting service

--- greeting-service/src/test/java/io/pivotal/pivotalservices/greeting/GreetingServiceApplicationTests.java
+++ greeting-service/src/test/java/io/pivotal/pivotalservices/greeting/GreetingServiceApplicationTests.java
@@ -5,8 +5,12 @@ import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.cloud.contract.stubrunner.StubTrigger;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,14 +20,19 @@ import java.util.List;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureStubRunner(ids = {"io.pivotal.pivotalservices:customer-service:+:stubs:9090"}, workOffline = true)
 public class GreetingServiceApplicationTests {
 
-	@Autowired
+	@SpyBean
 	private CustomerService customerService;
+
+	@Autowired
+	private StubTrigger stubTrigger;
 
 	@Test
 	public void contextLoads() {
@@ -43,10 +52,27 @@ public class GreetingServiceApplicationTests {
 		List<Customer> newCustomers = customerService.getNewCustomers();
 
 		// then
+		verify(customerService).sendEmail(eq(newCustomers.get(0)));
+
 		assertThat(newCustomers.size(), equalTo(1));
 		assertThat(newCustomers.get(0).getFirstName(), equalTo("Shaun"));
 		assertThat(newCustomers.get(0).getLastName(), equalTo("Anderson"));
 		assertThat(newCustomers.get(0).getCreatedOn(), equalTo(newCustomerCreatedOn));
+	}
+
+	@Test
+	public void testCustomerServiceGetsNotifiedOfNewCustomers() {
+		// given
+
+		// when
+		stubTrigger.trigger("newCustomerAdded");
+
+		// then
+		ArgumentCaptor<NewCustomerEvent> argument = ArgumentCaptor.forClass(NewCustomerEvent.class);
+		verify(customerService).newCustomerAdded(argument.capture());
+		assertThat(argument.getValue().getCustomerId(), equalTo("bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"));
+
+		verify(customerService).sendEmail(Matchers.any());
 	}
 
 }
